8cb6b81f462eacb225003afd42569418cfb0f57f
Gabriel Gusso <gabrielpgusso@gmail.com>
2024-09-19 21:48:22 -0300

    update gh link

--- src/app/_components/social-label/SocialLabel.tsx
+++ src/app/_components/social-label/SocialLabel.tsx
@@ -7,7 +7,7 @@ import GhIcon from "@/assets/icons/gh.svg";
 export default function SocialLabel() {
 	return (
 		<div className={styles.wrapper}>
-			<a href="" target="_blank">
+			<a href="https://github.com/g-pg/tech-test-todo" target="_blank">
 				<GhIcon />
 			</a>
 			<a href="https://www.linkedin.com/in/ggusso/" target="_blank">
